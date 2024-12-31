27867d7ecbbc215705c1a1f453167e3dec3aed44
Muhammed Bayhan <muhammedbayhan72@gmail.com>
2024-12-31 22:10:55 +0300

    footer added

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import "@ant-design/v5-patch-for-react-19";
 import Header from "@/components/header";
+import Footer from "@/components/footer";
 
 export const metadata: Metadata = {
   title: "Rick and Morty Universe",
@@ -18,6 +19,7 @@ export default function RootLayout({
       <body>
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
